ba776d54049d1ac2cc62fc721e433d218e082d03
kamranzafar4343 <kamranzafar4343@gmail.com>
2025-11-23 00:53:22 +0500

    display todos from db

--- resources/views/livewire/todo-manager.blade.php
+++ resources/views/livewire/todo-manager.blade.php
@@ -4,11 +4,24 @@ use Livewire\Volt\Component;
 
 // server code
 new class extends Component {
-    //
+    public function with()
+    {
+        return[
+            'todos' => Auth::user()->todos()->latest()->get(),
+    ];
+    }
 }; ?>
 
 
 {{-- html --}}
 <div>
-    //
+    @foreach ($todos as $todo)
+     <div>
+    <li>
+        {{ $todo->title }}   
+    </li>
+    
+    </div>   
+        
+    @endforeach
 </div> 
