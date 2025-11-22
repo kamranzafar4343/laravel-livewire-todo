219b466966927d78df58cab31429aa5bba42d7c1
kamranzafar4343 <kamranzafar4343@gmail.com>
2025-11-23 01:26:36 +0500

    added functions for create and deleted

--- resources/views/livewire/todo-manager.blade.php
+++ resources/views/livewire/todo-manager.blade.php
@@ -1,9 +1,31 @@
 <?php
 
 use Livewire\Volt\Component;
+use App\Models\Todo;
 
 // server code
 new class extends Component {
+    public Todo $todo;
+    public string $todoName = '';
+
+    public function createTodo(){
+        $this->validate([
+            'todoName' => 'required|min:3',
+        ]);
+
+        Auth::user()->todos()->create([
+            'title' => $this->todoName,
+        ]);
+
+        // $this->todoName = '';
+    }
+    
+    public function delete($id){
+        $todo = Todo::find($id);
+        if($todo && $todo->user_id === Auth::id()){
+            $todo->delete();
+        }
+    }
     public function with()
     {
         return[
@@ -18,11 +40,20 @@ new class extends Component {
     <form wire:submit='createTodo'>
         <x-text-input wire:model='todoName' placeholder="Enter todo title" class="mr-2"/>
         <x-primary-button type="submit">Add Todo</x-primary-button>
+        <x-input-error :messages="$errors->get('todoName')" class="mt-2"/>
     </form>
     @foreach ($todos as $todo)
      <div>
         {{ $todo->title }}   
+<x-danger-button 
+    wire:click="delete({{ $todo->id }})" 
+    size="sm" 
+    class="mr-4 mt-4"
+>
+    Delete
+</x-danger-button>
+
     </div>   
-        <button wire:click='delete({{ $todo->id }})' class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete</button>
+
     @endforeach
 </div> 
